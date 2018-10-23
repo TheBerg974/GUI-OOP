9c2caec2285ac5ad911dd3ef79c127602b719a9f
TheBerg974 <32581690+TheBerg974@users.noreply.github.com>
2018-10-22 20:21:58 -0400

    SpotifyParser

---
+++ SpotifyParser/SpotifyParser/SpotifyParser/src/spotifyparser/SpotifyParser.java
@@ -0,0 +1,17 @@
+package spotifyparser;
+
+import java.util.ArrayList;
+
+public class SpotifyParser {
+
+    public static void main(String[] args) {
+        String artistId = SpotifyController.getArtistId("Lil Skies");
+        ArrayList<Album> albums = SpotifyController.getAlbumDataFromArtist(artistId);
+        
+        if (albums.size() > 0){
+            System.out.println(albums.get(0).toString());
+        }
+        
+    }
+    
+}
